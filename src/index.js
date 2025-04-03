6cdc256691e4d81bd945a5365e97bc3d7d1ed2d9
p-i-ki <pikisaikia12@gmail.com>
2025-04-03 14:00:24 +0530

    added sequelize

--- src/index.js
+++ src/index.js
@@ -10,3 +10,14 @@ app.use("/api", apiRoutes);
 app.listen(ServerConfig.PORT, () => {
   console.log(`Successfully started server on PORT: ${ServerConfig.PORT}`);
 });
+
+// ORM vs ODM
+// ORM is used to connect OOP language to Relational dbs like sql. ex -> sequelize
+// ODM is used to connect OOP langs with non-Relational dbs like mongoose . ex -> mongoose
+// Using this ORM and ODM we can use objects or object oriented syntext to directly interact with DBs..
+
+// Now we will use sequelize ORM here. for this wwe first need to install a driver to cummicate with a DB (like mysql,sqlite,mariadb) then install sequalize
+// Driver will helps ORM to communicate with a particular DB , different drivers required for diffrent DBs
+
+// Working with squalize is not so easy like working with those ORM rails or others.
+// To make our life easy we will use sequelize-cli ->
